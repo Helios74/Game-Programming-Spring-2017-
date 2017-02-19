2d1ff2eaddadd6861dc0010a6e507290e40d82a2
Helios74 <joalsa310@gmail.com>
2017-02-19 10:51:55 -0600

    Created an animation utilizing two threads, one to update the locations of all sprites contained within it, and the other to render the updates.

---
+++ assignment02/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public static Form form;
+        public static Thread UpdateT;
+        public static Thread RenderT;
+        public static List<Sprite> sprites = new List<Sprite>();
+        public static Sprite canvas = new Sprite();
+        Pic Ring = new Pic(Properties.Resources.ring,0);
+        Pic Tyson = new Pic(Properties.Resources.tyson1,0);
+        Pic Kaiser = new Pic(Properties.Resources.kaiser,180);
+        public static int fps = 60;
+        public static double runningFPS = 60.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            form = this;
+            Ring.CornerX = 0;
+            Ring.CornerY = 0;
+            Tyson.CornerX = ClientSize.Width / 2;
+            Tyson.CornerY = ClientSize.Height / 4;
+            Kaiser.CornerX = ClientSize.Width / 2;
+            Kaiser.CornerY = 3 * ClientSize.Height / 4;
+            Ring.Width = ClientSize.Width;
+            Ring.Height = ClientSize.Height;
+            Tyson.Width = ClientSize.Width / 8;
+            Tyson.Height = ClientSize.Height / 8;
+            Kaiser.Width = ClientSize.Width / 8;
+            Kaiser.Height = ClientSize.Height / 8;
+            canvas.add(Ring);
+            canvas.add(Tyson);
+            canvas.add(Kaiser);
+            sprites.Add(Tyson);
+            sprites.Add(Kaiser);
+            UpdateT = new Thread(new ThreadStart(move));
+            UpdateT.Start();
+            RenderT = new Thread(new ThreadStart(run));
+            RenderT.Start();
+        }
+
+        public void UpdateSize()
+        {
+            Ring.Width = ClientSize.Width;
+            Ring.Height = ClientSize.Height;
+            Tyson.CornerX = ClientSize.Width / 2;
+            Tyson.CornerY = ClientSize.Height / 4;
+            Kaiser.CornerX = ClientSize.Width / 2;
+            Kaiser.CornerY = 3 * ClientSize.Height / 4;
+            Ring.Width = ClientSize.Width;
+            Ring.Height = ClientSize.Height;
+            Tyson.Width = ClientSize.Width / 8;
+            Tyson.Height = ClientSize.Height / 8;
+            Kaiser.Width = ClientSize.Width / 8;
+            Kaiser.Height = ClientSize.Height / 8;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            UpdateSize();
+            Refresh();
+        }
+
+        public static void move()
+        {
+            while(true)
+            {
+                foreach (Pic k in sprites)
+                {
+                    k.act(form.ClientSize.Width, form.ClientSize.Height);
+                }
+            }
+        }
+
+        public void run()
+        {
+            while(true)
+            {
+                DateTime last = DateTime.Now;
+                DateTime now = last;
+                TimeSpan frameTime = new TimeSpan(10000000 / fps);
+                while (true)
+                {
+                    DateTime tem = DateTime.Now;
+                    runningFPS = 0.9 * runningFPS + 0.1 * (1000.0 / (tem - now).TotalMilliseconds);
+                    now = tem;
+                    TimeSpan diff = now - last;
+                    if (diff.TotalMilliseconds < frameTime.TotalMilliseconds)
+                    {
+                        Thread.Sleep((frameTime - diff).Milliseconds);
+                    }
+                    last = DateTime.Now;
+                    form.Invoke(new MethodInvoker(form.Refresh));
+                }
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            UpdateT.Abort();
+            RenderT.Abort();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            canvas.render(e.Graphics);
+        }
+    }
+}
---
+++ assignment02/WindowsFormsApplication2/Pic.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public class Pic:Sprite
+    {
+        public float theta;
+        public bool reverse = false;
+        public int Offset;
+
+        float cornerX;
+
+        public float CornerX
+        {
+            get { return cornerX; }
+            set { cornerX = value; }
+        }
+
+        float cornerY;
+
+        public float CornerY
+        {
+            get { return cornerY; }
+            set { cornerY = value; }
+        }
+
+        public Pic(Image image, int offset)
+        {
+            this.Image = image;
+            this.cornerX = .5f;
+            this.cornerY = .5f;
+            this.Offset = offset;
+        }
+
+        public override void paint(Graphics g)
+        {
+            g.DrawImage(this.Image, new Rectangle((int)CornerX, (int)CornerY, (int)Width, (int)Height));
+        }
+
+        public override void act(int length, int width)
+        {
+            if(reverse == false)
+            {
+                theta += 0.000001F;
+            }
+            else
+            {
+                theta -= 0.000001F;
+            }
+            if(Math.Abs((Math.PI/2)-theta) < 0.01)
+            {
+                reverse = true;
+            }
+            else if(Math.Abs(0 - theta) < 0.01)
+            {
+                reverse = false;
+            }
+            CornerX = width/2+(float)(50 * Math.Cos(theta+Offset));
+            CornerY = length/2+(float)(50 * Math.Sin(theta+Offset));
+        }
+    }
+}
